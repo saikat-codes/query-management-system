f290d21ce7480d5ecf62d620aacf921627eb1653
saikat-codes <saikat612006@gmail.com>
2026-05-13 21:44:18 +0530

    add basic folder structure and in frontend add Tailwind CSS and Vite dependencies to package.json

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello world!
+      </h1>
+
+
+    </>
+  )
+}
+
+export default App
